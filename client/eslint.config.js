0e68c52206252516ec01440ea244fe50a8efff8b
Saksham <saksham77779@gmail.com>
2026-02-05 03:08:02 +0530

    Remove placeholder comment in eslint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-//change later
+
 export default defineConfig([
   globalIgnores(['dist']),
   {
